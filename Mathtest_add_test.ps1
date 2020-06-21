08caa01272d09fb24d1422071b26cd6f2c8426b6
T8z5h3 <38236420+T8z5h3@users.noreply.github.com>
2020-06-21 09:32:26 -0300

    a working test of the Adding part.
    a simple demo of the basic Adding script

---
+++ Mathtest_add_test.ps1
@@ -0,0 +1,30 @@
+﻿$roundCount = 1
+$rightanswers = 0
+$wronganswers = 0
+cls
+do {
+            write-host "this is Round: $roundCount"
+            $num1 =  Get-Random -Maximum 10 -Minimum 1
+            $num2 = Get-Random -Maximum 10 -Minimum 1
+        
+            $MathAnswer = $num1 + $num2
+            $MathAnswer
+
+
+            write-host "$num1 + $num2 ="
+            $typedAnswer = Read-Host "what is the answer?"
+            
+            if ($MathAnswer -eq $typedAnswer) {
+            cls
+            Write-Host "The Answer was Correct!"
+            $rightanswers ++
+            }
+            else {
+            cls
+            Write-Host "your answer is wrong." 
+            $wronganswers ++
+            }
+         $roundCount ++  
+    read-Host "Hit any Key to Continue."
+    cls 
+    } until ($roundCount -eq 11)
